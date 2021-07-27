8f17116070e25e20ca1ad8c26fd9af7087f2610d
OlivierVermesse <olivier.vermesse74@gmail.com>
2021-07-27 07:51:12 +0100

    Correction next signup

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,12 @@ mongoose.connect('mongodb+srv://Admin:P6Admin2021@cluster0.b0g5s.mongodb.net/myF
 
 //ajout de cette application afin de dire à l'API qu'elle est public et les actions possible à faire par le front
 // app.use((req, res, next) => { //on ne met pas d'addresse de route afin qu'elle fonctionne pour tous
-app.use((req, res) => { 
+app.use((req, res, next) => { 
   res.setHeader('Access-Control-Allow-Origin', '*'); //le * dit que l'on autorise tout le monde
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // on dit qu'elle en tête le front a le droit de voir
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //on dit quelles actions le front a le droit de faire
-//   next(); //ne pas oublier le next pour passer à la suivante
+  next(); //ne pas oublier le next pour passer à la suivante
+// res.json({message: "OK recu"})
 });
 
 //ajout du chemin de la route que les js devront prendre
