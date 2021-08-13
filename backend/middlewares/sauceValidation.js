254981e9d1d283c002da34c6bf17f8a0dfc06f97
OlivierVermesse <olivier.vermesse74@gmail.com>
2021-08-13 08:24:47 +0100

    ajout validateur de saisie

--- backend/middlewares/sauceValidation.js
+++ backend/middlewares/sauceValidation.js
@@ -3,7 +3,41 @@ const validate = require('mongoose-validator');
 exports.nameValidator = [
   validate({
     validator: 'isLength',
-    arguments: [15, 60],
+    arguments: [3, 50],
+    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+  }),
+  validate({
+    validator: 'matches',
+    arguments: /^[a-zA-Z0-9 -]+$/,
+    message: "Vous ne pouvez utiliser que des chiffres et des lettres",
+  }),
+];
+
+exports.manufacturerValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3, 50],
+    message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
+  }),
+  validate({
+    validator: 'matches',
+    arguments: /^[a-zA-Z0-9 -]+$/,
+    message: "Vous ne pouvez utiliser que des chiffres et des lettres",
+  }),
+];
+
+exports.descriptionValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3],
+    message: 'Name should be between {ARGS[0]} characters',
+  }),
+];
+
+exports.pepperValidator = [
+  validate({
+    validator: 'isLength',
+    arguments: [3, 50],
     message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters',
   }),
   validate({
--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,15 +1,14 @@
 //import de mongoose
 const mongoose = require("mongoose");
-const validationField = require('../middlewares/sauceValidation');
-
+const validate = require('../middlewares/sauceValidation');
 
 //création du schéma de données
 const saucesSchema = mongoose.Schema ({ //grace a Schema on va pouvoir créer la base via un objet
     userId: {type: String, required:true},
-    name: {type: String, required:true, validate: validationField.nameValidator},
-    manufacturer: {type: String, required:true},
-    description: {type: String, required:true},
-    mainPepper: {type: String, required:true},
+    name: {type: String, required:true, validate: validate.nameValidator },
+    manufacturer: {type: String, required:true, validate: validate.manufacturerValidator},
+    description: {type: String, required:true, validate: validate.descriptionValidator},
+    mainPepper: {type: String, required:true, validate: validate.pepperValidator},
     imageUrl: {type: String, required:true},
     heat: {type: Number, required:true},
     likes: {type: Number},
